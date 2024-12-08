eabd944c3a745a90c2098e4b42604fbb13a81c9e
Parkhomenko Egor <parkhomenko2049@gmail.com>
2024-12-08 19:02:16 +0600

    [Feat] Init project

---
+++ src/main/java/com/lovejazz/gymsession/demo/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.lovejazz.gymsession.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
---
+++ src/test/java/com/lovejazz/gymsession/GymsessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lovejazz.gymsession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymsessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
